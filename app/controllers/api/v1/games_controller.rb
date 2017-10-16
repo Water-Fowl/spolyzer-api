74510338a1fd6eee077a5e5f3eeb12d7fb445908
kuromike0629 <kuromike0629@gmail.com>
2017-10-17 00:56:37 +0900

    [fix]カラム変更に伴う、パラメータのチェックを変更

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,8 +3,9 @@ class Api::V1::GamesController < ApplicationController
   def create
     #Permit Parameters
     user = params.require(:game).permit(:user_id,:token)
-    game_info = params.require(:game).require(:score).permit(:left_team_id,:right_team_id,:game_time,:serve_user_id,:match_point,:deuce,:left_1,:left_2,:right_1,:right_2,:point_left,:point_right)
-    game_score = params.require(:game).require(:score).permit(:action => [],:position => [],:time => [],:scores_users => [],:conceded_users =>[])
+    players = params.require(:game).require(:score).permit(:left_user_id_1,:left_user_id_2,:right_user_id_1,:right_user_id_2)
+    game_info = params.require(:game).require(:score).permit(:left_team_id,:right_team_id,:game_time,:serve_user_id,:match_point,:deuce,:last_score_of_left,:last_score_of_right)
+    game_score = params.require(:game).require(:score).permit(:action => [],:position => [],:time_to_drop_shuttle => [],:scores_users => [],:conceded_users =>[])
     #User Authorization
     ##Write Here....
 
@@ -12,14 +13,14 @@ class Api::V1::GamesController < ApplicationController
     g = ScoreGame.create(game_info)
 
     #Retate with users table
-    ScoreGameToUser.create(user_id:game_info["left_1"],score_game_id:g.id)
-    ScoreGameToUser.create(user_id:game_info["left_2"],score_game_id:g.id)
-    ScoreGameToUser.create(user_id:game_info["right_1"],score_game_id:g.id)
-    ScoreGameToUser.create(user_id:game_info["right_2"],score_game_id:g.id)
+    ScoreGameToUser.create(user_id:players["left_user_id_1"],score_game_id:g.id)
+    ScoreGameToUser.create(user_id:players["left_user_id_2"],score_game_id:g.id)
+    ScoreGameToUser.create(user_id:players["right_user_id_1"],score_game_id:g.id)
+    ScoreGameToUser.create(user_id:players["right_user_id_2"],score_game_id:g.id)
 
     #Registor Scores
-    game_score["action"].zip(game_score["position"],game_score["time"],game_score["scores_users"],game_score["conceded_users"]) do |a|
-      req = {action:a[0],position:a[1],time:a[2],scores_user_id:a[3],conceded_user_id:a[4],score_game_id:g.id}
+    game_score["action"].zip(game_score["position"],game_score["time_to_drop_shuttle"],game_score["scores_users"],game_score["conceded_users"]) do |a|
+      req = {action:a[0],position:a[1],time_to_drop_shuttle:a[2],scores_user_id:a[3],conceded_user_id:a[4],score_game_id:g.id}
       Score.create(req)
     end
     render json: {status:0,message:"ok"}
