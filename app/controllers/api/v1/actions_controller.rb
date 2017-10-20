2ef4af59c9df4192ac17c224b08cb302b20c65e1
yusuke7futsal <yuhsukeshootsfc@gmail.com>
2017-10-20 19:45:09 +0900

    [add] 内部で集計するアルゴリズムを書いた

--- app/controllers/api/v1/actions_controller.rb
+++ app/controllers/api/v1/actions_controller.rb
@@ -4,8 +4,16 @@ class Api::V1::ActionsController < ApplicationController
     position = params_position["position"]
     # 該当するscore_gameを取得
     score_game = ScoreGame.find(params_position[:score_game_id])
-    scores_by_position = score_game.score.where(position: position)
-    
-    render :json => scores_by_position
+    actions_by_position = score_game.score.where(position: position).select("action").to_a
+    actions_list = [] 
+    for hash in actions_by_position
+        actions_list.push(hash["action"])        
+    end
+    action_values = actions_list.uniq
+    action_count_hash = {}
+    for action_value in action_values
+        action_count_hash[action_value] =  actions_list.count(action_value)
+    end
+    render :json => action_count_hash
   end
 end
