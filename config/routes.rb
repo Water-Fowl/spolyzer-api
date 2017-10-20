19d6bcd6e6f0f870c99387ecf901ef8929e5af6b
yusuke7futsal <yuhsukeshootsfc@gmail.com>
2017-10-21 00:41:29 +0900

    [add] User認証のroutesの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+       mount_devise_token_auth_for 'User', at: 'auth'
        post 'show_by_position', controller: 'actions'
        resources :games
        resources :actions
     end
   end
 
-  mount_devise_token_auth_for 'User', at: 'auth'
   resources :score_game_to_users
   resources :scores
   resources :score_games
