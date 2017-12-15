40a8f012bdfedba9ccb195178b11c3315ea9d7a9
takumimuggle <matsuot1101@keio.jp>
2017-12-15 20:59:46 +0900

    deleted original code-forgot to do that earlier sorry

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ Rails.application.routes.draw do
         mount_devise_token_auth_for 'User', at: 'auth', controllers: {
             registrations: 'api/v1/auth/registrations'
         }
-        post 'show_by_position', controller: 'actions'
         get  'actions/:position/:side/:score_game_id' to 'actions#show'
-        post 'show_by_status', controller: 'actions'
         get  'games/:user_id/:opponent_user/:victory' to 'games#show'
-        post 'search_user_by_id', controller: 'users'
         get  'users/:name' to 'users#search_user_by_id'
         resources :games
         resources :actions
