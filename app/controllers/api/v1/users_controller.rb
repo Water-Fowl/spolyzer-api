7a8c23b56f10feb1acf73c5601d9e788439aa7a5
takumimuggle <matsuot1101@keio.jp>
2017-12-15 21:42:20 +0900

    changed the parameter formats in the controllers(users)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
-class Api::V1::UsersController < ApplicationController  
+class Api::V1::UsersController < ApplicationController
   before_action :authenticate_api_v1_user!
 
   def search_user_by_id
       params_name = params.require(:data).permit(:name)
-      @name = params_name["name"]
+      @name = params[:name]
       users = User.where('name LIKE ?', "%#{@name}%")
       if not users.empty?
         render json: {status:0, message:"ok", is_exist: true, users: users}
