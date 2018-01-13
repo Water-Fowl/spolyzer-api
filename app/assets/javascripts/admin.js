bfc60617be8a5f5550c230ef798677fd60607656
100010 <yohei.senju@gmail.com>
2018-01-13 15:19:13 +0900

    [add] Init assets for admin, Load it

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require metisMenu/jquery.metisMenu.js
+//= require pace/pace.min.js
+//= require peity/jquery.peity.min.js
+//= require slimscroll/jquery.slimscroll.min.js
+//= require iCheck/icheck.min.js
+//= require flash
+//= require inspinia.js
+//= require table_sort
---
+++ app/assets/javascripts/flash.coffee
@@ -0,0 +1,8 @@
+#= require toastr
+
+$(document).ready ->
+  toastr.options =
+    closeButton: true
+    progressBar: true
+    showMethod: 'slideDown'
+    timeOut: 4000
