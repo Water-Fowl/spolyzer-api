92833a3772b8aae33764f5b5405f5058f7359ac0
100010 <yohei.senju@gmail.com>
2018-02-10 16:41:32 +0900

    [change] Use devise auth for all api controllers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BaseController < ApplicationController
+  before_action :authenticate_api_v1_user!
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   protect_from_forgery with: :null_session
