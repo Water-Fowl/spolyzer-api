fc2a6b0665b1e492aea244c5d826937c55ab7b61
100010 <yohei.senju@gmail.com>
2018-02-16 01:06:18 +0900

    [add] Touch users controller spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+  describe 'GET #show' do
+    it 'should return user detail' do
+      binding.pry
+    end
+  end
+end
