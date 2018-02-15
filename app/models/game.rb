82cc3be19d9c5184bb27fc62d02734c52e29939d
100010 <yohei.senju@gmail.com>
2018-02-16 02:19:23 +0900

    [add] Implement winner logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,23 @@
 class Game < ApplicationRecord
   has_many :game_users
-  has_many :users, through: :match_users
+  has_many :users, through: :game_users
   has_many :scores
+
+  def winner
+    user_order_by_score.last
+  end
+
+  def loser
+    user_order_by_score.first
+  end
+
+  private
+
+  def user_order_by_score
+    @order ||= self
+      .users
+      .sort do |user|
+        user.scores.where(game_id: self.id).count
+      end
+  end
 end
