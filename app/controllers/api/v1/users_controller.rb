00c15592e921ff4b27cfceeff87c880fa732d277
100010 <yohei.senju@gmail.com>
2018-02-16 11:27:39 +0900

    [change] Implement users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,11 +9,20 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def update
-    binding.pry
-    current_user =
+    if current_user.update(user_params)
+      render :show, status: :success
+    else
+      render json: { err_msg: current_user.errors.full_messages.join("\n") }, status: :bad_request
+    end
   end
 
   private def find_users_by_name
     @users = User.where('name LIKE ?', "%#{params[:name]}%")
+  end
+
+  private def user_params
+    params.permit(
+     :name
+   )
   end
 end
