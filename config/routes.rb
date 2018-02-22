774a40e9c5d16416d2c020d72016613ed5aede77
yamad07 <yuhsukeshootsfc@gmail.com>
2018-02-23 02:45:34 +0900

    [add] positionsのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   namespace :api, format: 'json' do
     namespace :v1 do
         mount_devise_token_auth_for 'User', at: 'auth', controllers: {
           registrations: 'api/v1/auth/registrations'
         }
+        resources :positions
         resources :shot_types
         resources :games
         resources :users
