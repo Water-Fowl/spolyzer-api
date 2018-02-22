e102136b77ab29cf3472e4cc36ae8023d82b9bbe
100010 <yohei.senju@gmail.com>
2018-02-22 10:51:50 +0900

    [change] Tweak env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  user_name: 'waterfowl',
+  password: 'PASSWORD',
+  domain: '',
+  address: '',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+ActionMailer::Base.raise_delivery_errors = true
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,9 @@
 Devise.setup do |config|
   # The e-mail address that mail will appear to be sent from
   # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
-  config.mailer_sender = ENV['INFO_MAIIL']
+
+  # TODO: envにまとめる
+  config.mailer_sender = 'yuhsukeshootsfc@gmail.com'
   config.secret_key = '4295bdc7f545fdfb21c49d93216f683045f9daf7f454ef46d8e60523821d96902b830b01c0320c2ead71c39a92729098647fe8aa77b0af4f7ae65da722437362'
   # If using rails-api, you may want to tell devise to not use ActionDispatch::Flash
   # middleware b/c rails-api does not include it.
