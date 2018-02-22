aa0c94ab7c1c37b37ecfc490c119bb871b559f74
早坂大翔 <HayasakaHiroto@mac.local>
2018-02-22 15:08:57 +0900

    [fix] sport.rbの,の抜けを修正 games/show.json.jbuilderのshottypeを修正

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -3,6 +3,6 @@ class Sport < ApplicationRecord
   has_many :sport_users
   has_many :users, through: :sport_users
 
-  validates :dropped_at presence: true
-  validates :side presence: true
+  validates :dropped_at, presence: true
+  validates :side, presence: true
 end
