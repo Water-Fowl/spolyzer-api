ef1298fc87f9a1131eb3208c292aecca9513004d
早坂大翔 <HayasakaHiroto@mac.local>
2018-02-23 16:55:25 +0900

    [fix] バリデーションの記述漏れを修正

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -4,8 +4,10 @@ class Unit < ApplicationRecord
 
   has_many :user_units
   has_many :users, through: :user_units
-end
 
-def game_units_and_user_units_equal?(game_id, unit_id_1, unit_id_2)
-  GameUnit.where(game_id: game_id).where(unit_id: unit_id_1).count == GameUnit.where(game_id: game_id).where(unit_id: unit_id_1).count
+  validates: game_units_and_user_units_equal?
+
+  def game_units_and_user_units_equal?(game_id, unit_id_1, unit_id_2)
+    GameUnit.where(game_id: game_id).where(unit_id: unit_id_1).count == GameUnit.where(game_id: game_id).where(unit_id: unit_id_1).count
+  end
 end
