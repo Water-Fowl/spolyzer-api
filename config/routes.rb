d9422bf26219759d56c73ea1facf6d2a4ecd57ac
yamad07 <yuhsukeshootsfc@gmail.com>
2018-02-24 00:20:15 +0900

    [change] positions_controller内でcountsを定義するように変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
         mount_devise_token_auth_for 'User', at: 'auth', controllers: {
           registrations: 'api/v1/auth/registrations'
         }
-        namespace :positions do
-          get 'counts', to: 'counts#show'
+        resources :positions do
+          collection do
+            get :counts
+          end
         end
         resources :shot_types
         resources :games
