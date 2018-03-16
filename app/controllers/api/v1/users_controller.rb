fbdd159621e522a9762e056a9d25162624c19215
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-16 20:24:30 +0900

    [add] userのコントローラーを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,18 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   before_action :find_users_by_name, only: [:search]
 
+  def index
+  end
+
   def show
+    @user = User.find(params[:id])
   end
 
   def search
   end
 
   def update
-    current_user.update(user_params)
+    current_api_v1_user.update(user_params)
   end
 
   private def find_users_by_name
