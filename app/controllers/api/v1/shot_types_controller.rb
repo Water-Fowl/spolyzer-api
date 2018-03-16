341c90703f3a7cec20b46e53e804bbedca84a238
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-16 20:23:00 +0900

    [add] 試合後にshot_typeを数えるコードの追加

--- app/controllers/api/v1/shot_types_controller.rb
+++ app/controllers/api/v1/shot_types_controller.rb
@@ -1,7 +1,27 @@
 class Api::V1::ShotTypesController < Api::V1::BaseController
 
+  def index
+    @shot_types = ShotType.where(sport_id: params[:sport_id])
+  end
   def show
     @shot_type = @sport.shot_types
+  end
+
+  def counts
+    @count = CountShotTypeService.new(params).execute
+#    game = Game.find params[:id]
+#    @count = game
+#      .scores
+#      .joins(:position)
+#      .group('positions.side', 'positions.dropped_at', 'scores.shot_type_id', 'scores.miss_type')
+#      .count.reduce({}) do |count, (key, value)|
+#      side, dropped_at, shot_type, miss_type = key
+#      count[side] ||= {}
+#      count[side][dropped_at] ||= {}
+#      count[side][dropped_at][shot_type] ||= {}
+#      count[side][dropped_at][shot_type][miss_type] = value
+#      count
+#    end
   end
 
   private def set_sport
