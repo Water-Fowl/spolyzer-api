826a247fbef99f410049b469c067395d65f1fc5e
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-16 20:17:12 +0900

    [add] Userを追加するコードの追加

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   def sign_up_params
     params.permit(:name, :email, :password, :password_confirmation)
   end
+
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+    end
+  end
 end
