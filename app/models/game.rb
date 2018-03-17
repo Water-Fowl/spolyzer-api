2f152acf08e08237820c5a46a77bd9dcfffe420c
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-17 14:04:23 +0900

    [add] Unit経由でGameを取るロジックの追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -21,4 +21,28 @@ class Game < ApplicationRecord
         user.scores.where(game_id: self.id).count
       end
   end
+
+  scope :of_user, -> (user_id){
+    joins(:users)
+    .where(users: {id: user_id})
+  }
+
+  scope :of_doubles_users, -> (user_ids){
+    joins(:units)
+      .where(units: {id: Unit
+      .of_doubles_users(user_ids)
+      .pluck(:id)
+    })
+  }
+
+  scope :of_singles_opponent_user, -> (user_id, opponent_user_id){
+    joins(:units)
+      .where(units: {id: Unit
+      .of_user(opponent_user_id)
+      .where.not(id: user_id)
+      .pluck(:id)
+    })
+    .of_user(user_id)
+  }
+
 end
