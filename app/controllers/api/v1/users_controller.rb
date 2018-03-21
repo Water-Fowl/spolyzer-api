ee35eeae9236c17e630ef84c0c1b6549dd1c3cca
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-22 02:20:41 +0900

    [add] 画像をbase64からデコードして保存するコードを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,10 +21,28 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   private def user_params
-    params.permit(
+    user_params = params.permit(
      :name,
      :email,
      :image
-   )
+    )
+    user_params[:image] = parse_image_data(user_params[:image]) if user_params[:image]
+    user_params
+  end
+
+  private def parse_image_data(base64_image)
+    filename = "profile-image"
+    in_content_type, encoding, string = base64_image.split(/[:;,]/)[1..3]
+
+    @tempfile = Tempfile.new(filename)
+    @tempfile.binmode
+    @tempfile.write Base64.decode64(string)
+    @tempfile.rewind
+
+    ActionDispatch::Http::UploadedFile.new({
+      tempfile: @tempfile,
+      content_type: in_content_type,
+      filename: filename
+    })
   end
 end
