330e819ff2909f37ca7230028ab1f5182799aad9
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-22 02:26:35 +0900

    [add] mailを環境変数で指定できるようにした

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  user_name: 'waterfowl',
-  password: 'PASSWORD',
-  domain: '',
-  address: '',
+  user_name: ENV['MAIL_USER_NAME'],
+  password: ENV['MAIL_PASSWORD'],
+  domain: ENV['MAIL_DOMAIN'],
+  address: ENV['MAIL_ADDRESS'],
   port: 587,
   authentication: :plain,
   enable_starttls_auto: true
