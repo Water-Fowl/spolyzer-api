55d2804820ce9ed30af01a760655e7d879db4d0a
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-22 02:29:00 +0900

    [add] configのgemを追加

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+
+  if Rails.env.development?
+    config.asset_host = 'http://localhost:3000'
+  end
+  if Rails.env.production?
+    config.fog_directory  = 'spolyzer-user'
+    config.cache_storage = :fog
+  end
+end
+
