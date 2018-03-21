ef674c114ae490a96b0ed644904d1e480b1bc229
yamad07 <yuhsukeshootsfc@gmail.com>
2018-03-22 02:33:53 +0900

    [remove] コメントアウト部分を削除

--- app/controllers/api/v1/shot_types_controller.rb
+++ app/controllers/api/v1/shot_types_controller.rb
@@ -9,19 +9,6 @@ class Api::V1::ShotTypesController < Api::V1::BaseController
 
   def counts
     @count = CountShotTypeService.new(params).execute
-#    game = Game.find params[:id]
-#    @count = game
-#      .scores
-#      .joins(:position)
-#      .group('positions.side', 'positions.dropped_at', 'scores.shot_type_id', 'scores.miss_type')
-#      .count.reduce({}) do |count, (key, value)|
-#      side, dropped_at, shot_type, miss_type = key
-#      count[side] ||= {}
-#      count[side][dropped_at] ||= {}
-#      count[side][dropped_at][shot_type] ||= {}
-#      count[side][dropped_at][shot_type][miss_type] = value
-#      count
-#    end
   end
 
   private def set_sport
