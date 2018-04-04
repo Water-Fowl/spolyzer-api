0cca7bc1dfef639548292f0bde94a0f0cd03fd89
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-04 12:38:04 +0900

    [add] dot-envの導入

--- capistrano/config/deploy/production.rb
+++ capistrano/config/deploy/production.rb
@@ -1,5 +1,5 @@
 server '128.199.68.200:57854', user: fetch(:user), roles: %w{ web app db worker cron }, primary: true
-set :branch, 'master'
+set :branch, 'v1.0.0'
 set :stage, :production
 set :rails_env, :production
 set :ssh_keys, ['~/.ssh/id_rsa']
