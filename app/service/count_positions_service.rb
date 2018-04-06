379426e2c5ee999b6cd506614b6116e7274fccb9
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-06 16:47:49 +0900

    [add] 対戦相手からcountsをするコードを追加

--- app/service/count_positions_service.rb
+++ app/service/count_positions_service.rb
@@ -5,6 +5,20 @@ class CountPositionsService
     @user = user
   end
 
+  def counts_by_opponent_users
+    opponent_user_ids = @params[:user_ids].split(",").map{|id| id.to_i }
+    opponent_users = User.find(opponent_user_ids)
+    game_user_count = @params[:game_user_count]
+    scores = Score
+      .of_opponent_users_games(@user, opponent_users, game_user_count)
+      .where(shot_type_id: @params[:shot_type_id])
+      .where(miss_type: 0)
+      .joins(:position)
+    counts = {}
+    counts[0] = reshape(scores.of_user_units(@user))
+    counts[1] = reshape(scores.of_not_user_units(@user))
+  end
+
   def execute
     opponent_user_ids = @params[:user_ids].split(",").map{|id| id.to_i }
     opponent_users = User.find(opponent_user_ids)
