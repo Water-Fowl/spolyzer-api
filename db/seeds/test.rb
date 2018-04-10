9855355e4f16fc03d686a22e887b8c8bc8689ca2
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-10 17:16:11 +0900

    [add] testのときにseedデータを入れるためのスクリプトを追加

---
+++ db/seeds/test.rb
@@ -0,0 +1,23 @@
+ActiveRecord::Base.transaction do
+  Sport.create(
+    name_ja: 'バドミントン',
+    name_en: 'badminton'
+  )
+  sport = Sport.first
+  [
+    ['ヘアピン', 'hairpin'],
+    ['スマッシュ', 'smash'],
+    ['クリアー', 'clear'],
+    ['ドライブ', 'drive'],
+    ['プッシュ', 'push'],
+    ['ロブ', 'rob'],
+    ['サービス', 'service'],
+    ['ドロップ', 'drop'],
+  ].each do |type|
+    ShotType.create(
+      name_ja: type.first,
+      name_en: type.second,
+      sport_id: sport.id
+    )
+  end
+end
