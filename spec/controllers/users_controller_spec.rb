dbb47a619fac5374f46ec2cf69c2cecf35cc480c
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-10 17:14:31 +0900

    [add] rspecによるcontrollerのtestの追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
   describe 'GET #show' do
     it 'should return user detail' do
-      binding.pry
     end
   end
 end
