86585d78e4694fec0046955cab8dd3c873d067c2
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-14 20:03:38 +0900

    [add] baseとなるcontrollerのテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :user do
-    4.times.each do |i|
-      name "test#{i}"
-      email "test#{i}@test.com"
-      password "test#{i}"
-    end
+    name "test"
+    email "test@test.com"
+    password "testtesttest"
   end
 end
