a1dcc3bf34a1981593185ecfc1b83975cf6aeb3c
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-14 10:06:49 +0900

    [fix] Positionsモデルの削除に伴うFactoriesのコードの変更

--- spec/factories/scores.rb
+++ spec/factories/scores.rb
@@ -2,23 +2,14 @@ FactoryBot.define do
   factory :left_score, class: Score do
     miss_type 0
     shot_type_id 1
+    dropped_at 1
+    dropped_side 1
   end
 
   factory :right_score, class: Score do
     miss_type 0
     shot_type_id 2
+    dropped_at 2
+    dropped_side 0
   end
-
-  trait :with_left_position do
-    after :create do |score|
-      create(:left_position, score)
-    end
-  end
-
-  trait :with_right_position do
-    after :create do |score|
-      create(:right_position, score)
-    end
-  end
-
 end
