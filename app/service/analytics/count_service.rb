fe2e1e29abc3de3022099162fa7c99e09eee4469
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-16 17:54:35 +0900

    [fix] bagfix

--- app/service/analytics/count_service.rb
+++ app/service/analytics/count_service.rb
@@ -3,7 +3,7 @@ module Analytics
     def execute
       scores = Score.of_user_games(@user, @params[:game_user_count])
         .where(shot_type_id: @params[:shot_type_id])
-        .where(miss_type: 0)
+        .where(is_net_miss: false)
       counts = {}
       counts[0] = reshape(scores.of_not_user_units(@user))
       counts[1] = reshape(scores.of_user_units(@user))
