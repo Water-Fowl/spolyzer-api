96b2047c07848134d0358e612831b7c7596636c8
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-24 00:18:49 +0900

    [add] stagingのブランチの変更

--- capistrano/config/deploy/staging.rb
+++ capistrano/config/deploy/staging.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 server '52.199.120.130', user: fetch(:user), roles: %w[web app db worker cron], primary: true
-set :branch, 'v1.0.0'
+set :branch, 'feature/aws-cap'
 set :stage, :staging
 set :rails_env, :staging
 set :ssh_keys, ['~/.ssh/id_rsa']
