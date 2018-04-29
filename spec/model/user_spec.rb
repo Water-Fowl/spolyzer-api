53e9e719d8bd063cd09154e38cc6628bcfe11fe3
HirotoHayasaka <hayahiro19@gmail.com>
2018-04-29 14:03:25 +0900

    [add] バリデーションに関するテストを追加

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -9,6 +9,18 @@ RSpec.describe User, :type => :model do
       @user2 = Unit.second.users.first
     end
 
+    let(:user) {User.new(email: "test@test.com")}
+    
+    it "nameが入っていないと無効" do
+      expect(user).to_not be_valid
+    end
+
+    let(:user) {User.new(name: "test")}
+
+    it "emailが入っていないと無効" do
+      expect(user).to_not be_valid
+    end
+
     describe "latest_result" do
       it "最新の分析結果を検索" do
         #expect(@user1.latest_result.ids).to eq  @user1.analysis_results.order(created_at: :asc).ids
