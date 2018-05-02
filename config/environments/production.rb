67c961e8d7bf7a836f9fe7678485162e9c7c5c6e
yamad07 <yuhsukeshootsfc@gmail.com>
2018-05-02 18:21:19 +0900

    [add] productionのホストをハードコード

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "api_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: ENV["PRODUCTION_HOST"] }
+  config.action_mailer.default_url_options = { host: '54.249.94.110' }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
