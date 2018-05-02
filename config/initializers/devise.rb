236f2ae7a54773ee75c6ba8970e8bd0c6b4cef7d
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-02 14:50:50 +0900

    [add] メールの本文ファイルの追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,7 @@ Devise.setup do |config|
   # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
 
   # TODO: envにまとめる
+  config.mailer = 'Users::Mailer'
   config.mailer_sender = ENV["MAIL_USER_NAME"]
   config.secret_key = '4295bdc7f545fdfb21c49d93216f683045f9daf7f454ef46d8e60523821d96902b830b01c0320c2ead71c39a92729098647fe8aa77b0af4f7ae65da722437362'
   # If using rails-api, you may want to tell devise to not use ActionDispatch::Flash
