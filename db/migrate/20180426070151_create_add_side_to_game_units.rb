aa61b31844728356af468f373169c3234f866120
yamad07 <yuhsukeshootsfc@gmail.com>
2018-05-02 15:27:04 +0900

    [delte] 不要なmigrationファイルの削除

--- db/migrate/20180426070151_create_add_side_to_game_units.rb
+++
@@ -1,9 +0,0 @@
-class CreateAddSideToGameUnits < ActiveRecord::Migration[5.1]
-  def change
-    create_table :add_side_to_game_units do |t|
-      t.integer :side
-
-      t.timestamps
-    end
-  end
-end
