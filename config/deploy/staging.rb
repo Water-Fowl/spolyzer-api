31065ba2185db8cdd8ae1f1f466e670c59dd529b
yamad07 <yuhsukeshootsfc@gmail.com>
2018-05-10 14:32:24 +0900

    [add] デプロイの際に参照するssh-keyの変更

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ server '52.199.120.130', user: fetch(:user), roles: %w[web app db worker cron], 
 set :branch, 'development/v1.1.0'
 set :stage, :staging
 set :rails_env, :staging
-set :ssh_keys, ['~/.ssh/id_rsa']
+set :ssh_keys, ['deploy_key']
 set :ssh_options,
     keys: fetch(:ssh_keys).map { |x| File.expand_path(x) },
     forward_agent: true
