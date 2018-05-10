1f2350f3a6f3f0a21344650230c1c40a12cec647
yamad07 <yuhsukeshootsfc@gmail.com>
2018-05-10 14:56:56 +0900

    [fix] デプロイする際に参照するssh-keyを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ server '54.249.94.110', user: fetch(:user), roles: %w[web app db worker cron], p
 set :branch, 'master'
 set :stage, :production
 set :rails_env, :production
-set :ssh_keys, ['~/.ssh/id_rsa']
+set :ssh_keys, ['deploy_key']
 set :ssh_options,
     keys: fetch(:ssh_keys).map { |x| File.expand_path(x) },
     forward_agent: true
