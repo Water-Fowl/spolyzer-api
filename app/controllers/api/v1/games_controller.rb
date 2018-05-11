573446fcd45cdf2aa80d3358c63eda891ece9eea
yamad07 <yuhsukeshootsfc@gmail.com>
2018-05-11 23:36:24 +0900

    [add] service patternを、createのメソッドのロジックの際に追加

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -13,10 +13,10 @@ class Api::V1::GamesController < Api::V1::BaseController
 
     create_game
 
-    @left_unit = find_or_create_units(:left)
-    @right_unit = find_or_create_units(:right)
+    left_unit, right_unit = CreateUnitsService.call(units_params, @game.id)
 
-    create_scores
+    CreateScoresService.call(scores_params, @game.id, left_unit, right_unit)
+
     @game.update_outcome
 
   end
@@ -27,41 +27,15 @@ class Api::V1::GamesController < Api::V1::BaseController
     @game = Game.create(name: game_params[:name], sport_id: @sport.id)
   end
 
-  def find_or_create_units(side)
-
-    user_count = @units[side][:count]
-
-    users = []
-    user_count.times do |i|
-      user = @units[side][:users][i]
-      users.push(User.find(user[:id]))
-    end
-
-    unit = Unit.find_or_create_with_users(users)
-    unit.game_units.create(game_id: @game.id, side: side)
-    unit
-  end
-
-  def create_scores
-    @scores.each do |score|
-      if score[:is_net_miss]
-        unit_id = score[:side] == 1 ? @right_unit.id : @left_unit.id
-      else
-        unit_id = score[:side] == 1 ? @left_unit.id : @right_unit.id
-      end
-
-      @score = Score.create(
-        unit_id: unit_id,
-        is_net_miss: score[:is_net_miss],
-        shot_type_id: score[:shot_type],
-        position_id: score[:dropped_at],
-        dropped_side: score[:side],
-        game_id: @game.id
-      )
-    end
-  end
-
   def game_params
     params.require(:game).permit(:name)
+  end
+
+  def units_params
+    params.require(:units)
+  end
+
+  def scores_params
+    params.require(:scores)
   end
 end
---
+++ app/services/create_units_service.rb
@@ -0,0 +1,48 @@
+class CreateUnitsService
+  include BaseService
+  attr_reader :params
+
+  def initialize(params, game_id)
+    @params = params
+    @game_id = game_id
+  end
+
+  def call
+    left_unit = create_units(:left)
+    right_unit = create_units(:right)
+    return left_unit, right_unit
+  end
+
+  private
+
+  def create_units(side)
+    unit = find_or_create_with_users(side)
+    unit.game_units.create(game_id: @game_id, side: side)
+    unit
+  end
+
+  def set_users(side)
+    user_count = params[side][:count]
+    users = []
+    user_count.times do |i|
+      user = params[side][:users][i]
+      users.push(User.find(user[:id]))
+    end
+    users
+  end
+
+  def find_or_create_with_users(side)
+    users = set_users(side)
+    unit = Unit.joins(:users).where(users: {id: users.pluck(:id)}).first
+
+    if not unit
+      unit = Unit.create(user_count: users.count)
+      users.each do |user|
+        unit.user_units.create(user_id: user[:id])
+      end
+    end
+
+    unit
+  end
+
+end
