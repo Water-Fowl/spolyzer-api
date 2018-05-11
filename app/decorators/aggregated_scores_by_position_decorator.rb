8b4885882b4b9baeb45d324676a4d8305b6d4f22
yamad07 <yuhsukeshootsfc@gmail.com>
2018-05-11 16:06:41 +0900

    [add] decorator patternの導入

---
+++ app/decorators/aggregated_scores_by_position_decorator.rb
@@ -0,0 +1,22 @@
+class AggregatedScoresByPositionDecorator
+  include BaseDecorator
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def call
+    if !scores.empty?
+      scores.group('scores.position_id').count.reduce({}) do |count, (key, value)|
+        dropped_at = key
+        count ||= {}
+        count[dropped_at] ||= value
+        count
+      end
+    else
+      []
+    end
+  end
+
+end
