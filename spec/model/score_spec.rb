7fcabe7d4ee28b3062ac35aa4ca0a2552f4df574
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-16 16:36:44 +0900

    [fix] score_spec.rbの整理

--- spec/model/score_spec.rb
+++ spec/model/score_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Score", :type => :model do
-  before(:each) do
-    @unit = create(:unit)
-    @game = create(:game)
-    @unit.games << @game
-    @game.units << create(:unit)
-    @user = create(:user)
+
+  let(:unit) { create(:unit) }
+  let(:game) { create(:game) }
+  let(:user) { create(:user) }
+
+  before  do
+    unit.games << game
+    game.units << create(:unit)
     Unit.first.users << create(:user)
     Unit.second.users << create(:user)
   end
+
   describe "scopes" do
 
-    describe "of_opponent_users_games" do
-      it "user vs opponent_users の試合におけるスコアを返す" do
-        expect(Score.of_opponent_users_games(Unit.first.users.first, Unit.second.users, 1).pluck(:id)).to eq @game.scores.pluck(:id)
-      end
-    end
 
     describe "of_user_games" do
       it "userが含まれている試合のscoreを返す" do
