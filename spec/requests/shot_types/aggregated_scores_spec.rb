52e3f8ce96d31db95ad52ab7455d6880f02ff572
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-16 18:00:41 +0900

    [fix] テストデータなどのリファクタリング

--- spec/requests/shot_types/aggregated_scores_spec.rb
+++ spec/requests/shot_types/aggregated_scores_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "shot_types/AggregatedScore", type: :request do
+
+  let(:user) { create(:user) }
+
   before do
     unit = create(:unit)
     game = create(:game)
@@ -9,18 +12,16 @@ RSpec.describe "shot_types/AggregatedScore", type: :request do
     opponent_unit = create(:unit)
     opponent_unit.games << game
 
-    @user = create(:user)
-
     opponent_user = create(:user)
 
-    unit.users << @user
+    unit.users << user
     opponent_unit.users << opponent_user
 
     score =  Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1)
     game.scores << score
     #TODO 共通処理として切り出す
     @headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
-    auth_header = @user.create_new_auth_token
+    auth_header = user.create_new_auth_token
     @headers.merge! auth_header
   end
 
@@ -31,15 +32,14 @@ RSpec.describe "shot_types/AggregatedScore", type: :request do
           opponent_users_ids: "2"
         }}
 
-      let(:current_api_v1_user){@user}
+      let(:current_api_v1_user){user}
 
       subject do
         get "/api/v1/shot_types/:shot_type_id/aggregated_scores",  params: params, as: :json,headers: @headers
       end
 
-      it "return 200" do
-        subject
-        expect(response).to have_http_status(200)
+      it "ステータスコード200を返す" do
+        is_expected.to have_http_status(200)
       end
 
       it "shot_type毎の集計を送る" do
