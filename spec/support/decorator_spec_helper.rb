5b0a178efcfa41cf827d18cfbc3e45e890f5f6b6
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-17 14:04:46 +0900

    [fix] 読みにくさの改善

---
+++ spec/support/decorator_spec_helper.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'rails_helper'
+
+module DecoratorSpecHelper
+  def self.included(base)
+    base.class_eval do
+      before do
+        controller = ApplicationController.new
+        controller.request = ActionDispatch::TestRequest.create
+        ActiveDecorator::ViewContext.push controller.view_context
+      end
+    end
+  end
+end
