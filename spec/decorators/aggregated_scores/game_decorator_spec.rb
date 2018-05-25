5073f1fb1397437011caf457ae296c1e8425ab73
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-25 14:40:18 +0900

    [fix] インデントの修正

--- spec/decorators/aggregated_scores/game_decorator_spec.rb
+++ spec/decorators/aggregated_scores/game_decorator_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'GameDecorator', :type => :decorator do
     context "scoresが存在する場合" do
 
       let(:unit) { create(:unit) }
-        let(:other_unit) { create(:unit) }
-        let(:game) { create(:game, :with_scores, units: [unit, other_unit]) } 
-        let(:scores) { create_list(:scores, 10, position: Position.first, shot_type: ShotType.first, game: game, unit: unit)}   
+      let(:other_unit) { create(:unit) }
+      let(:game) { create(:game, :with_scores, units: [unit, other_unit]) } 
+      let(:scores) { create_list(:scores, 10, position: Position.first, shot_type: ShotType.first, game: game, unit: unit)}   
 
       it 'side,dropped_at,shot_type,miss_typeによってscoreを分類する' do
         expect(AggregatedScores::GameDecorator.call(game.scores)).not_to be_empty 
