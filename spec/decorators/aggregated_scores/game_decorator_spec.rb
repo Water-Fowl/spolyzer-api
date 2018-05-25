a64b0930e56d9e4191b63ac7ea3ac525114ab9f3
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-25 12:06:19 +0900

    [fix] テストがgreenになるように修正

--- spec/decorators/aggregated_scores/game_decorator_spec.rb
+++ spec/decorators/aggregated_scores/game_decorator_spec.rb
@@ -2,15 +2,13 @@ require "rails_helper"
 
 RSpec.describe 'GameDecorator', :type => :decorator do
 
-  let(:game) { create(:game) }
-  let(:scores) { [Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: 1, position_id: 1, is_net_miss: false),Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: 1, position_id: 1, is_net_miss: false)] }
-
   describe '#call' do
     context "scoresが存在する場合" do
 
-      before do
-        game.scores << scores 
-      end 
+      let(:unit) { create(:unit) }
+        let(:other_unit) { create(:unit) }
+        let(:game) { create(:game, :with_scores, units: [unit, other_unit]) } 
+        let(:scores) { create_list(:scores, 10, position: Position.first, shot_type: ShotType.first, game: game, unit: unit)}   
 
       it 'side,dropped_at,shot_type,miss_typeによってscoreを分類する' do
         expect(AggregatedScores::GameDecorator.call(game.scores)).not_to be_empty 
@@ -18,8 +16,11 @@ RSpec.describe 'GameDecorator', :type => :decorator do
     end
 
     context "scoresが存在しない場合" do
+
+      let(:scores) { [] }
+
       it '空の配列を返す' do
-        expect(AggregatedScores::GameDecorator.call(game.scores)).to be_empty
+        expect(AggregatedScores::GameDecorator.call(scores)).to be_empty
       end
     end
   end
