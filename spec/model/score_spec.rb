c2524a9855d0acd3a113e5b1fdc239bd3270f994
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-25 16:59:54 +0900

    [fix] model/score_spec をレビュー後に修正

--- spec/model/score_spec.rb
+++ spec/model/score_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe "Score", :type => :model do
 
   let(:unit) { create(:unit) }
-  let(:not_user_unit) { create(:unit) }
+  let(:opponent_unit) { create(:unit) }
   let(:game) { create(:game) }
   let(:user) { create(:user) }
-  let(:not_user) { create(:user) }
+  let(:opponent_user) { create(:user) }
   let(:score) { Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1, is_net_miss: false) }
-  let(:not_user_unit_score) { Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: not_user_unit.id, position_id: 1, is_net_miss: false) }
+  let(:opponent_unit_score) { Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: opponent_unit.id, position_id: 1, is_net_miss: false) }
 
   before  do
     unit.games << game
-    game.units << not_user_unit
+    game.units << opponent_unit
     unit.users << user
-    not_user_unit.users << not_user
-    game.scores << [score, not_user_unit_score]
+    opponent_unit.users << opponent_user
+    game.scores << [score, opponent_unit_score]
   end
 
   describe "scopes" do
@@ -35,7 +35,7 @@ RSpec.describe "Score", :type => :model do
 
     describe "of_not_user_units" do
       it "userが含まれていないunitのscoreを返す" do
-        expect(Score.of_not_user_units(user).pluck(:id)).to eq not_user_unit.scores.pluck(:id)
+        expect(Score.of_not_user_units(user).pluck(:id)).to eq opponent_unit.scores.pluck(:id)
       end
     end
 
