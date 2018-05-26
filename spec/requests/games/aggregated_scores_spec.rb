42fe9acbb9ec1c385dd423192b226e56cd51b8ab
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-26 12:53:00 +0900

    [fix] 返ってきたjsonの中身のテストを場合分け

--- spec/requests/games/aggregated_scores_spec.rb
+++ spec/requests/games/aggregated_scores_spec.rb
@@ -5,11 +5,9 @@ RSpec.describe "games/AggregatedScore", type: :request do
   let(:game) { create(:game) }
   let(:user) { create(:user) }
   let(:unit) { create(:unit) }
-  let(:score) { Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1) }
 
   before do
     unit.games << game 
-    game.scores << score
 
     #TODO 共通処理として切り出す
     @headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
@@ -18,10 +16,15 @@ RSpec.describe "games/AggregatedScore", type: :request do
   end
 
   describe "GET /api/v1/games/:game_id/aggregated_scores　#index" do
+
       let(:params) do
         {
           game_id: game.id
         }
+      end
+
+      before do
+        game.scores << scores
       end
 
       subject do
@@ -33,9 +36,37 @@ RSpec.describe "games/AggregatedScore", type: :request do
         expect(response).to have_http_status(200)
       end
 
-      it "scoreの集計を送る" do
-        subject
-        expect(json['counts']).not_to be_empty
+
+      context "全て同じscoreの場合" do
+        let(:scores) { [Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1)]}
+      
+        it "scoreの集計を送る" do
+          subject
+          expect(json['counts'].count).to eq 1
+        end
       end
+      
+      context "drppped_side: 0が２つ、1が３つの場合" do
+        let(:scores) { [Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 0, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 0, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1)]}
+      
+        it "scoreの集計を送る" do
+          subject
+
+          
+          binding.pry
+          
+          expect(json['counts'].first.count).to eq 2 
+        end
+      end
+
+      context "shot_type_idが全て異なる場合" do
+        let(:scores) { [Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 2, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 3, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 4, dropped_side: 1, unit_id: unit.id, position_id: 1), Score.create(game_id: game.id, shot_type_id: 5, dropped_side: 1, unit_id: unit.id, position_id: 1)]}
+      
+        it "scoreの集計を送る" do
+          subject
+          expect(json['counts']['1']['1'].length).to eq 5
+        end
+      end
+      
   end
 end
