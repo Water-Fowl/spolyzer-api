d3ab59a1a91fb3aea40cf121ffb6c9a450471eb2
yamad07 <yuhsukeshootsfc@gmail.com>
2018-05-28 01:06:49 +0900

    [fix] gameをcreateするときのsideのロジックを変更

--- app/services/create_scores_service.rb
+++ app/services/create_scores_service.rb
@@ -12,11 +12,6 @@ class CreateScoresService
 
   def call
     params.each do |score_params|
-      if score_params[:is_net_miss]
-        unit_id = score_params[:side] == 1 ? right_unit.id : left_unit.id
-      else
-        unit_id = score_params[:side] == 1 ? left_unit.id : right_unit.id
-      end
       Score.create(
         game_id: game_id,
         unit_id: unit_id,
