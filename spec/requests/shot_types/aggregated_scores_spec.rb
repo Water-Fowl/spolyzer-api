4f66a9f534c4b4be9e4dc43673873ddf9f899cb6
HirotoHayasaka <hayahiro19@gmail.com>
2018-05-28 13:09:36 +0900

    [fix] テストデータの整理

--- spec/requests/shot_types/aggregated_scores_spec.rb
+++ spec/requests/shot_types/aggregated_scores_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe "shot_types/AggregatedScore", type: :request do
   let(:opponent_user) { create(:user) }
   let(:unit) { create(:unit) }
   let(:game) { create(:game) }
+  let!(:game_unit) { GameUnit.create(unit_id: unit.id, game_id: game.id, side: 0, outcome: 2) }
   let(:opponent_unit) { create(:unit) }
   let(:score) { Score.create(game_id: game.id, shot_type_id: 1, dropped_side: 1, unit_id: unit.id, position_id: 1) }
 
   before do
-    game = create(:game)
-    unit.games << game
     opponent_unit.games << game
     unit.users << user
     opponent_unit.users << opponent_user
@@ -25,16 +24,19 @@ RSpec.describe "shot_types/AggregatedScore", type: :request do
   end
 
   describe "GET /api/v1/shot_types/:shot_type_id/aggregated_scores #index" do
-      let(:params){{
-          game_user_count: 2,
-          shot_type_id: 1,
+      let(:params) do
+      {
+          game_user_count: 1,
+          shot_type_id: "1",
           opponent_users_ids: [opponent_user.id],
-          created_after: Time.now,
-          created_before: "018-05-09 05:31:55",
+          created_after: "#{game_unit.created_at}",
+          created_before: "#{game_unit.created_at}",
           game_ids: [1],
           shot_type_id: 1,
-          is_net_miss: true
-        }}
+          is_net_miss: true,
+          outcome: 2
+        }
+      end
 
       let(:current_api_v1_user){user}
 
@@ -47,7 +49,7 @@ RSpec.describe "shot_types/AggregatedScore", type: :request do
       end
 
       it "shot_type毎の集計を送る" do
-        subject
+        subject        
         expect(json['counts']).not_to be_empty
       end
   end
