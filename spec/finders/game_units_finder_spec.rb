d0c9df8dd0cff4c842d043c130a24e3c81fbc987
HirotoHayasaka <hayahiro19@gmail.com>
2018-06-01 17:20:31 +0900

    [fix] game_unitsのテストデータを複数にした

--- spec/finders/game_units_finder_spec.rb
+++ spec/finders/game_units_finder_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'GameUnitsFinder', :type => :finder do
   let(:opponent_unit) { create(:unit) }
   let(:current_user) { create(:user) }
   let(:opponent_user) { create(:user) }
-  let!(:game_unit) { GameUnit.create(game_id: game.id, unit_id: unit.id, side: 1, outcome: 2) } 
+  let!(:game_units) { [GameUnit.create(game_id: game.id, unit_id: unit.id, side: 1, outcome: 2),GameUnit.create(game_id: game.id, unit_id: unit.id, side: 0, outcome: 2)] } 
   let!(:user_unit) { UserUnit.create(unit_id: unit.id, user_id: current_user.id) }
   let!(:game_opponent_unit) { GameUnit.create(game_id: game.id, unit_id: opponent_unit.id, side: 0, outcome: 0) } 
   let!(:opponent_user_unit) { UserUnit.create(unit_id: opponent_unit.id, user_id: opponent_user.id) }
@@ -18,14 +18,13 @@ RSpec.describe 'GameUnitsFinder', :type => :finder do
             opponent_users_ids: opponent_user.id.to_s,
             game_user_count: 1,
             outcome: 2,
-            created_after: game_unit.created_at.to_s,
-            created_before: game_unit.created_at.to_s
+            created_after: game_units.second.created_at.to_s,
+            created_before: game_units.first.created_at.to_s
         }
     end
-
-    it '対戦相手、ダブルスシングルス、勝敗、登録された日時でGameUnitを絞り込む' do
-      expect(GameUnitsFinder.call(current_user, params).pluck(:id)).to eq [game_unit.id] 
+    
+    it 'return GameUnit searched by opponent_user, doubles or singles, outcome, and date_time' do
+      expect(GameUnitsFinder.call(current_user, params).pluck(:id)).to eq game_units.pluck(:id) 
     end
-  end  
-  
+  end
 end
