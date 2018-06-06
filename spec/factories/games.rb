2d21adccf478b3d9fb3722ffc981d3d07e73655f
HirotoHayasaka <hayahiro19@gmail.com>
2018-06-06 15:17:23 +0900

    [fix] 1ラインを短くした

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -13,8 +13,22 @@ FactoryBot.define do
     end
 
     after :create do |game, evaluator|
-      create_list(:left_score, evaluator.left_side_scores_count, position: Position.first, shot_type: ShotType.first, game: game, unit: game.units.first)
-      create_list(:right_score, evaluator.right_side_scores_count, position: Position.first, shot_type: ShotType.first, game: game, unit: game.units.second)
+      create_list(
+        :left_score,
+        evaluator.left_side_scores_count,
+        position: Position.first,
+        shot_type: ShotType.first,
+        game: game,
+        unit: game.units.first
+      )
+      create_list(
+        :right_score,
+        evaluator.right_side_scores_count,
+        position: Position.first,
+        shot_type: ShotType.first,
+        game: game,
+        unit: game.units.second
+      )
     end
   end
 end
